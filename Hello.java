b7ed6c0484c43942fab92444ccf8ccc4cdd32990
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 16:22:27 +0530

    hdsd

--- Hello.java
+++ Hello.java
@@ -3,9 +3,11 @@ class Hello{
 public static void main(String[]args){
 Scanner kb=new Scanner(System.in);
 int a=kb.nextInt();
-int b=kb.nextInt();
+int j=kb.nextInt();
+
 
 System.out.println("Addition "+(a+b));
+System.out.println("Substraction "+(a-b));
 
 
 
