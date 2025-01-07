0b0177f831d9aa5b90647cd5c60e230b11b9dc1e
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-01-07 16:12:28 +0530

    fsadf

--- Hello.java
+++ Hello.java
@@ -1,12 +1,12 @@
 import java.util.*;
-class Addition{
+class Hello{
 public static void main(String[]args){
 Scanner kb=new Scanner(System.in);
 int a=kb.nextInt();
 int b=kb.nextInt();
-int add=0;
-add=a+b;
 
-System.out.println("Addition "+add);
+
+
+
 }
 }
